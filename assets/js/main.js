6e683edb697cabeaea75f135a48ae4e2e92fa538
Raphael Garcia <42500954+Raposones@users.noreply.github.com>
2022-12-01 01:52:52 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,7 @@ function showDetails(champDets) {
 	getAPI.getChampionDetails(champDets).then(champion => {
 		const details = document.getElementById('modal-details')
 		
-		document.querySelector('.modal').style.backgroundImage = `linear-gradient(to bottom, rgba(7, 11, 37, 0.7) 100%, rgba(5, 7, 23, 0.9) 100%), url(/resources/champion_splash/${champion.name}_0.jpg`
+		document.querySelector('.modal').style.backgroundImage = `linear-gradient(to bottom, rgba(7, 11, 37, 0.7) 100%, rgba(5, 7, 23, 0.9) 100%), url(resources/champion_splash/${champion.name}_0.jpg`
 		document.querySelector('.modal #iconDiv').innerHTML = `<img id="icon" src="resources/champion_sprites/${champion.icon}" alt="${champion.icon}">`
 		document.querySelector('.modal #name').innerHTML = champion.name
 		document.querySelector('.modal #title').innerHTML = champion.title
